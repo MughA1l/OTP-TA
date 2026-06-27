1baa307cd97f210da47324b2da16201c0faa2694
Taj Mahal Developer <developer@tajmahal.com>
2026-06-28 00:54:41 +0500

    Update email validator regex to support ampersand and plus symbols

--- otp_ta_app/lib/core/utils/validators.dart
+++ otp_ta_app/lib/core/utils/validators.dart
@@ -5,7 +5,7 @@ abstract class Validators {
     if (value == null || value.trim().isEmpty) {
       return 'Email is required.';
     }
-    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    final emailRegex = RegExp(r'^[\w-\.&+]+@([\w-]+\.)+[\w-]{2,4}$');
     if (!emailRegex.hasMatch(value.trim())) {
       return 'Enter a valid email address.';
     }
